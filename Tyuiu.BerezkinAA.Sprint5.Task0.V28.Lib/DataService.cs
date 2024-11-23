123112d97e54ea4ed82723c20e27dbe7519184a8
Berezkin Artem <berezkin.artjom@yandex.ru>
2024-11-24 00:57:27 +0500

    Task0

--- Tyuiu.BerezkinAA.Sprint5.Task0.V28.Lib/DataService.cs
+++ Tyuiu.BerezkinAA.Sprint5.Task0.V28.Lib/DataService.cs
@@ -8,7 +8,7 @@ namespace Tyuiu.BerezkinAA.Sprint5.Task0.V28.Lib
             string path = Path.GetTempFileName();
 
             double z = (double)x * Math.Sqrt((double)x + 3);
-            z = Math.Round(z, 2);
+            z = Math.Round(z, 3);
             File.WriteAllText(path, Convert.ToString(z));
             return path;
         }
